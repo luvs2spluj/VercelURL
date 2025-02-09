64076b8062cf9a9345a063b3c393406bf55ce4b7
luvs2spluj <horton.alexander@gmail.com>
2025-02-09 01:52:32 -0800

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return(
    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
     <div>
-      <h1>Hello World</h1>
+      Hello World
     </div>
    </main>
   );
